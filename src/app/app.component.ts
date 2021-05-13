d4fda1c4d50174c439bf9d81ce3a6244340e8e03
anusuyabera <anusuya7bera@gmail.com>
2021-05-13 19:11:16 +0530

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'Anusuya Bera'
   dogcount = 1
-
+  dogs = [
+    {
+      name: "Jimmy",
+      activity: [
+        "walk",
+        "eat",
+        "bite people"
+      ],
+      image: "https://th.bing.com/th/id/OIP.EkOxo19lVUaq6eVlVPxZ2wHaE7?w=239&h=180&c=7&o=5&dpr=1.25&pid=1.7"
+    },
+    {
+      name: "Tommy",
+      activity: [
+        "sleep",
+        "eat",
+        "swim"
+      ],
+      image: "https://th.bing.com/th/id/OIP.Encpy4zhjNpIb0NAqlbLfwHaE7?w=239&h=180&c=7&o=5&dpr=1.25&pid=1.7"
+    },
+    {
+      name: "Brad",
+      activity: [
+        "walk",
+        "swim",
+        "chase cats"
+      ],
+      image: ""
+    }
+  ]
   addDog(){
     this.dogcount += 1
   }
